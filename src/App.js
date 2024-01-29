717721aa1c659b7ac685c2bd277366988e9d9877
Vulpe <cameroncarmichael@rogers.com>
2024-01-29 17:22:51 -0500

    added styles to nav bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Nav />
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Home} />
-        </Switch>
+          <Route path="/about" exact component={Home} />
+          <Route path='/channel' component={() => {
+            window.location.href = 'https://www.twitch.tv/vulpefox';
+            return null;
+          }}/>
+          <Route path="/projects" exact component={Home} />
+          <Route path="/connect" exact component={Home} />
+          
+          {/* default redirect to home page */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
       </Router>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import tailBite from '../assets/tailBite.png';
+
+import '../styles/Nav.css';
 
 function Nav() {
   return (
     <div className='nav'>
       <div className='vulpeLogo'>
-        <img src={tailBite}/>
+        <Link to="/"> 
+          <img src={tailBite}/>
+        </Link>
       </div>
-      <div className='dropdowns'></div>
+      <div className='dropdowns'>
+        <Link to="/about"> About </Link>
+        <Link to="/channel"> Channel </Link>
+        <Link to="/projects"> Projects </Link>
+        <Link to="/connect"> Connect </Link>
+      </div>
     </div>
   )
 }
