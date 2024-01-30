54bcc61f8287b9109a827b17d6e9e0167f248c87
Vulpe <cameroncarmichael@rogers.com>
2024-01-30 18:17:14 -0500

    funny first deploy

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,15 +9,26 @@ function About() {
   return (
     <div className="base">
       <div className="home1" style={{ backgroundImage: `url(${Image1})` }}>
-        <div className="headerContainer">
+        <div className="aboutContainer" id="first">
+          <h1>Howdy, I'm Vulpe!</h1>
+          <body>I'm some nerdy gay fox on the internet who streams some silly little games and math-based education. I go by she/they pronouns. So thanks for coming by, it's nice to meet you!
+          </body>
         </div>
       </div>
       <div className="home2" style={{ backgroundImage: `url(${Image2})` }}>
-        <div className="headerContainer">
+        <div className="aboutContainer" id="second">
+         <h1>About My Channel</h1>
+          <body>
+          I'm a streamer who values pawsitivity and maintaining a relaxed atmosphere during my streams. <br /> 
+          </body>
+          <body>
+            My focus is primarily on VRChat, but sometimes I like to unleash my wild side and play other games, too. My aim is to ensure that you feel right at home with us. You are valuable, and I appreciate you!
+          </body>
         </div>
       </div>
       <div className="home3" style={{ backgroundImage: `url(${Image3})` }}>
-        <div className="headerContainer">
+        <div className="aboutContainer" id="third">
+          <h1>Here is the ref of how I look ^w^</h1>
         </div>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 import HappyVulp from '../assets/forrogue.png';
 import HomeImage from '../assets/sky.png';
+
+import { TwitchPlayer, TwitchChat } from 'react-twitch-embed';
+
 import '../styles/Home.css';
 
 function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${HomeImage})` }}>
       <div className="headerContainer">
+        <img src={HappyVulp} alt=""/>
         <h1>Welcome to the official Vulpefox website!</h1>
         <body>Staying wholesome* since 2023</body>
-        <img src={HappyVulp} alt=""/>
+      </div>
+      <div className="twitchContainer">
+        <TwitchPlayer channel="vulpefox" id="woof" height="45vh" width="35vw" />
+        <TwitchChat channel="vulpefox" id="woof1" />
       </div>
     </div>
   );
