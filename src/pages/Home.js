86f05577c9790b7712c7dc4fddaa22ba74772dcd
Vulpe-Fox <lagopus39@gmail.com>
2024-12-24 04:03:00 -0500

    updated changes to twitch container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import HappyVulp from '../assets/forrogue.png';
 import HomeImage from '../assets/sky.jpg';
 
-import { TwitchPlayer, TwitchChat } from 'react-twitch-embed';
+import { TwitchPlayer/*, TwitchChat*/ } from 'react-twitch-embed';
 
 import '../styles/Home.css';
 
@@ -12,12 +12,12 @@ function Home() {
     <div className="home" style={{ backgroundImage: `url(${HomeImage})` }}>
       <div className="headerContainer">
         <img src={HappyVulp} alt=""/>
-        <h1>Welcome to the official Vulpefox website!</h1>
+        <h1>Welcome to the official Vulpe website!</h1>
         <body>Staying wholesome* since 2023</body>
       </div>
       <div className="twitchContainer">
-        <TwitchPlayer channel="vulpefoxy" id="woof" height="45vh" width="50%" />
-        <TwitchChat channel="vulpefoxy" id="woof1" />
+        <TwitchPlayer channel="vulpe" id="woof" height="45vh" width="100%" />
+        {/*<TwitchChat channel="vulpe" id="woof1" />*/}
       </div>
     </div>
   );
